d67a5373f729c26db7706326f6354e128dc1a6ba
Madhura Tayade <tayademadhura02@gmail.com>
2025-07-24 15:58:17 +0530

    feat/added-traffic-light-component-along-with-component-restructuring

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import { Stopwatch } from "./Stopwatch";
+import TrafficLight from "./components/TrafficLight/TrafficLight";
 
 function App() {
   return (
     <div className="App">
-      <Stopwatch />
+      <TrafficLight />
     </div>
   );
 }
